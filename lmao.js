917ebb480a1854adb9b73e6e755a78204794ad3f
CdottOTS <cbozeman4747@gmail.com>
2024-05-21 18:24:20 -0400

    Update lmao.js

--- lmao.js
+++ lmao.js
@@ -1,2 +1,6 @@
-
-alert('sdddddssssaaa')
+    if (Core < 1){
+    var KeyInput = window.prompt("Enter key","OTS/")
+    if (KeyInput.length == 21){
+    FullCheck()
+    }else WrongKey()
+}else alert("GUI has already been unlocked")
